37503d48c19ce04c039fcb4be181c00eda5538cb
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-06 16:35:52 -0300

    add Tweet component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import Tweet from '../Tweet'; 
+import Tweet from '../Tweet'; 
 
 import { Container, Tab, Tweets } from './styles';
 
@@ -9,10 +9,10 @@ const Feed: React.FC = () => {
     <Container>
       <Tab>Tweets</Tab>
       <Tweets>
-        {/* <Tweet/>
         <Tweet/>
         <Tweet/>
-        <Tweet/> */}
+        <Tweet/>
+        <Tweet/>
       </Tweets>
     </Container>
   );
