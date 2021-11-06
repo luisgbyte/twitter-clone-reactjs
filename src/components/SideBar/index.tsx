3bef84aba59f3e032325d2b8a429112a6484eb33
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-11-06 19:31:32 -0300

    add News component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
 
 import {
   Container,
@@ -18,21 +20,26 @@ const SideBar: React.FC = () => {
         <SearchInput placeholder="Burcar no Twitter" />
         <SearchIcon />
       </SearchWrapper>
-
-      <Body>
-        <List
-          title="Talvez você curta"
-          elements={[
-            <FollowSuggestion name="User1" nickname="@User1" />,
-            <FollowSuggestion name="User2" nickname="@User2" />,
-            <FollowSuggestion name="User3" nickname="@User3" />,
-          ]}
-        />
-        <List
-          title="Talvez você curta"
-          elements={[<h1>Test</h1>, <h1>Test</h1>, <h1>Test</h1>]}
-        />
-      </Body>
+      <StickyBox>
+        <Body>
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="User1" nickname="@User1" />,
+              <FollowSuggestion name="User2" nickname="@User2" />,
+              <FollowSuggestion name="User3" nickname="@User3" />,
+            ]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+        </Body>
+      </StickyBox>
     </Container>
   );
 };
